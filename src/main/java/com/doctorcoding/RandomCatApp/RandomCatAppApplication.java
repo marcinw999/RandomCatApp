cb9bcf3f215c2502cde899be1fbfea9ea5aeeda1
Wasiucionek <marcin.wasiucionek@gmail.com>
2020-02-08 11:27:30 +0100

    Initial commit

---
+++ src/main/java/com/doctorcoding/RandomCatApp/RandomCatAppApplication.java
@@ -0,0 +1,13 @@
+package com.doctorcoding.RandomCatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomCatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomCatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doctorcoding/RandomCatApp/RandomCatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctorcoding.RandomCatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomCatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
